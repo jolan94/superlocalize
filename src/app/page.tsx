0551ae63f7750f558f1846c594a906966d5d9f9d
Joe <jolan94@gmail.com>
2025-08-22 21:40:49 +0530

    feat: add homepage with header and landing page components

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Header } from "@/components/Header";
+import { LandingPage } from "@/components/LandingPage";
+
+export default function HomePage() {
+  return (
+    <>
+      <Header />
+      <LandingPage />
+    </>
+  );
+}
